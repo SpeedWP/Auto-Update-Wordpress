007b47f63ac63d7a2c2f055b8d9d63810446c9a4
speedwordpress <dani@speedword.press>
2017-04-09 15:24:47 +0200

    Autoupdate in Kürze

--- autoupdate.php
+++ autoupdate.php
@@ -2,16 +2,10 @@
 /*
 Plugin Name: Automatische WordPress Updates
 Plugin URI: https://speedword.press
-Description: Das Plugin muss sich im Ordner wp-content/mu-plugins/ befinden. Ebenfalls muss in der wp-config autoupdaten aktiviert sein.
+Description: Das Plugin muss sich im Ordner wp-content/mu-plugins/ befinden. Ebenfalls muss in der wp-config autoupdaten aktiviert sein. Zeile 10 updatet die Plugins, Zeile 11 die Themes aus dem offiziellen WordPress Respository. Einfach die jeweilige Zeile löschen,falls nur eines von Beidem sich selber updaten soll.
 Author: Daniel Bieli
 Author URI: https://speedword.press
 License: GPL2
 */
-
-//** Plugin Update */
-
 add_filter( 'auto_update_plugin', '__return_true' );
-
-//** Theme Auto Update */
- 
 add_filter( 'auto_update_theme', '__return_true' );
